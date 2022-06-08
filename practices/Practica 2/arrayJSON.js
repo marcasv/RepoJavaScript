14e5b5f79c4c5ae69f91ad646e9d810163cd72a7
Martí Castillo <marticastillo49@gmail.com>
2022-06-08 17:05:00 -0500

    Practicas 1 y 2

---
+++ practices/Practica 2/arrayJSON.js
@@ -0,0 +1,7 @@
+const jsonData= require('./archivo.json'); 
+let objects = jsonData.Objetos;
+console.log("\nObjects:");
+console.log(objects);
+console.log("\n\nKeys:");
+let keys = jsonData.Llaves;
+console.log(keys);
